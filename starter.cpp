13a10637be55b5481a929e204822c9946d5023f4
Anupam <onlyprogramming000@gmail.com>
2020-08-30 22:47:30 +0530

    Starter template C++

---
+++ starter.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+#define test  \
+	int t;    \
+	cin >> t; \
+	while (t--)
+
+#define f(i, c, n) for (int i = c; i < n; i++)
+#define ii int, int
+#define S string
+#define ll long long int
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define P pair
+#define US unordered_set
+#define UM unordered_map
+#define S set
+#define M map
+#define fi first
+#define sc second
+#define pb push_back
+#define endl '\n'
+
+#define Fast                           \
+	ios_base::sync_with_studio(false); \
+	cin.tie();                         \
+	cout.tie();
+
+int main()
+{
+	test
+	{
+		cout << "Hello World";
+	}
+	return 0;
+}
